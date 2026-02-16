61f551c4b8327c4192da69aa984b99ee911fc92e
Bryan <sktbryann@gmail.com>
2026-02-16 22:38:33 +0800

    Java Practices

---
+++ src/AsciiCodeFinder.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class AsciiCodeFinder {
+    public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a character: ");
+        char ch = input.next().charAt(0);
+
+        int asciiValue = (int) ch;
+
+        System.out.println("The ASCII code for character " + ch + " is " + asciiValue);
+
+    }
+}
