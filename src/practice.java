662c9dd6d8a015bd1acd49a87ad785dfbde3ebd0
Bryan <sktbryann@gmail.com>
2026-02-16 23:20:40 +0800

    Java Practices

--- src/practice.java
+++ src/practice.java
@@ -1,13 +1,80 @@
 import java.util.Scanner;
 public class practice{
     public static void main(String[]args){
+
+        String set1 = " 1  3  5  7\n" +
+                      " 9 11 13 15\n" +
+                      "17 19 21 23\n" +
+                      "25 27 29 31";
+
+        String set2 = " 2  3  6  7\n" +
+                      "12 13 14 15\n" +
+                      "18 19 22 13\n" +
+                      "26 27 30 31";
+
+        String set3 = " 4  5  6  7\n" +
+                      "12 13 14 15\n" +
+                      "20 24 22 23\n" +
+                      "28 29 30 31";
+
+        String set4 = "8  9 10 11\n" +
+                     "12 13 14 15\n" +
+                     "24 25 26 27\n" +
+                     "28 29 30 31";
+
+        String set5 = "16 17 18 19\n" +
+                      "20 21 22 23\n" +
+                      "24 25 26 27\n" +
+                      "28 29 30 31";
+        int day = 0;
+
         Scanner input = new Scanner(System.in);
 
-        System.out.print("Enter a character: ");
-        char random = input.next().charAt(0);
+        System.out.print("Is your birthday in Set1?\n");
+        System.out.println(set1);
+        System.out.print("Enter N if No and Y if Yes: ");
+        String answer = input.next();
 
-        int val = (int) random;
+        if(answer.equalsIgnoreCase("Y")){
+            day += 1;
+        }
 
-        System.out.println(val);
+        System.out.print("\nIs your birthday in Set2?\n");
+        System.out.println(set2);
+        System.out.print("Enter N if No and Y if Yes: ");
+        answer = input.next();
+
+        if(answer.equalsIgnoreCase("Y")){
+            day += 2;
+        }
+
+        System.out.print("\nIs your birthday in Set3?\n");
+        System.out.println(set3);
+        System.out.print("Enter N if No and Y if Yes: ");
+        answer = input.next();
+
+        if(answer.equalsIgnoreCase("Y")){
+            day += 4;
+        }
+
+        System.out.print("\nIs your birthday in Set4?\n");
+        System.out.println(set4);
+        System.out.print("Enter N if No and Y if Yes: ");
+        answer = input.next();
+
+        if(answer.equalsIgnoreCase("Y")){
+            day += 8;
+        }
+
+        System.out.print("\nIs your birthday in Set5?\n");
+        System.out.println(set5);
+        System.out.print("Enter N if No and Y if Yes: ");
+        answer = input.next();
+
+        if(answer.equalsIgnoreCase("Y")){
+            day += 16;
+        }
+
+        System.out.println("\nYour birthday is " + day + "!");
     }
 }
