8d02e6128180d3535d6452e5d702af58e7596a26
Bryan <sktbryann@gmail.com>
2026-02-19 00:02:49 +0800

    Converting letter to number

--- src/ConvertLetterToNumber.java
+++ src/ConvertLetterToNumber.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class practice {
+public class ConvertLetterToNumber {
     public static void main (String[]args){
         Scanner input = new Scanner(System.in);
 
