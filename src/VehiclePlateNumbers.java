ac410e0f8aba12283dfc77bb4105cc6ab299e383
Bryan <sktbryann@gmail.com>
2026-02-20 23:23:53 +0800

    Vehicle Plate Number generator

---
+++ src/VehiclePlateNumbers.java
@@ -0,0 +1,16 @@
+public class VehiclePlateNumbers {
+    public static void main(String[]args){
+
+        String plateNumber1 = "";
+        String plateNumber2 = "";
+
+        for(int i = 0; i < 3; i++){
+            char randomUpperCaseLetter = (char)((int)(Math.random() * 26) + 'A');
+            plateNumber1 += randomUpperCaseLetter;
+        }for(int i = 0; i < 4; i++){
+            int randomNumbers = ((int)(Math.random() * 10));
+            plateNumber2 += randomNumbers;
+        }
+        System.out.println("Your plate number is " + plateNumber1 + plateNumber2);
+    }
+}
