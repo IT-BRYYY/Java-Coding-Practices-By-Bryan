82e3225a17713f017c89a97e538a415d595455da
Bryan <sktbryann@gmail.com>
2026-02-25 23:38:01 +0800

    String Length and Last Character Extractor

---
+++ src/ProcessAString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class ProcessAString {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String userInput = input.nextLine().trim();
+
+        String length = String.valueOf(userInput.length());
+
+        char last = userInput.charAt(userInput.length() - 1);
+
+        System.out.println(length);
+        System.out.println(last);
+    }
+}
