19e9b13ef0b61738b09743e376fc70afc9659d01
Bryan <sktbryann@gmail.com>
2026-03-10 00:45:08 +0800

    Simple shopping cart with Y/N loop and receipt output

---
+++ src/SimpleShoppingCart.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class SimpleShoppingCart {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+
+        char continueLoop = 'Y';
+        int items = 0;
+        double sum = 0;
+
+
+        while(continueLoop == 'Y'){
+            System.out.print("Enter item price: ");
+            double price = input.nextDouble();
+            sum += price;
+            items ++;
+
+            System.out.printf("Items: %d | Total: %.2f%n", items , sum);
+            input.nextLine();
+
+            System.out.print("Enter Y to add more items and N to checkout: ");
+            continueLoop = input.nextLine().charAt(0);
+
+        }
+        System.out.println("==== RECEIPT ====");
+        System.out.printf("Total items: %d%n",items);
+        System.out.printf("Total Price: %.2f%n", sum);
+        System.out.println("=================");
+
+
+    }
+}
