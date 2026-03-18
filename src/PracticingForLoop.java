a154229c340a3a69a172c8b10f594f5cd53f9629
Bryan <sktbryann@gmail.com>
2026-03-18 23:27:13 +0800

    Random Practice using for loop

---
+++ src/PracticingForLoop.java
@@ -0,0 +1,18 @@
+public class PracticingForLoop {
+    public static void main (String[]args){
+
+
+        for(int i = 0, j = 10; i <=10 && j >=0; i++, j-- ){
+            System.out.println(i + " + " + j + " = " + (j + i));
+        }
+
+        for(int i = 1; i <100; i++){
+            for(int q = 1; q<= 100; q++) {
+                char random = (char) ('A' + (int) (Math.random() * 26));
+                System.out.print(random);
+
+            }
+            System.out.println();
+        }
+    }
+}
