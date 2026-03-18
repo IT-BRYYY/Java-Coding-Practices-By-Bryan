f7919ce859f5ad88641a77058c8a80ce76ff1538
Bryan <sktbryann@gmail.com>
2026-03-18 22:42:26 +0800

    Fixing some formatting in Multiplication Table

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,19 +1,18 @@
-public class MultiplicationTable {
+public class MultiplicationTable{
     public static void main(String[]args){
-        System.out.println("           Multiplication Table");
+        System.out.println("               Multiplication Table");
 
         System.out.print("    ");
-        for(int j = 1; j <= 9; j++)
-            System.out.print("   " + j);
+        for(int i = 1; i <=10; i++)
+            System.out.printf("%4d", i);
 
-        System.out.println("\n------------------------------------------");
-
-        for(int i = 1; i <= 9; i++){
-            System.out.print(i + " | ");
-            for(int j = 1; j <= 9; j++){
+        System.out.println("\n--------------------------------------------");
+        for(int j = 1; j<= 10; j++){
+            System.out.printf("%2d |", j);
+            for(int i = 1; i <= 10; i++){
                 System.out.printf("%4d", i * j);
             }
-            System.out.println();
+            System.out.println(" ");
         }
     }
 }
