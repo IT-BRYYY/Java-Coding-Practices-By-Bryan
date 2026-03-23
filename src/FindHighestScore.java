84c21e5f48d5d7222111a25ebd11b04f1fbe6642
Bryan <sktbryann@gmail.com>
2026-03-23 21:29:57 +0800

    Add program to find student with highest score using Scanner

---
+++ src/FindHighestScore.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class FindHighestScore {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many student: ");
+        int students = input.nextInt();
+
+        String nameOfHighestScore = "";
+        int highestScore = 0;
+
+        int score;
+        String name;
+
+        for(int i = students; i > 0; i--   ){
+
+            System.out.print("\nEnter name: ");
+            name = input.next();
+
+            System.out.print("Enter score: ");
+            score = input.nextInt();
+
+            if(score > highestScore){
+                highestScore = score;
+                nameOfHighestScore = name;
+            }
+        }
+
+
+        System.out.println("\nThe highest score is " + nameOfHighestScore + " with the score of " + highestScore);
+    }
+}
