4080c33e5e5af0d19278fa4ffd4a04081b4d83d2
Bryan <sktbryann@gmail.com>
2026-03-25 23:57:06 +0800

    Add program to compute monthly savings balance with deposits

---
+++ src/FinancialApplicationCompoundValue.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class FinancialApplicationCompoundValue {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        double monthlyInterest;
+        String output = "";
+        double total = 0;
+        int count = 0;
+
+        System.out.println("(Financial application: compound value)");
+
+        System.out.print("Enter an amount: ");
+        double amount = input.nextDouble();
+
+        System.out.print("Enter an interest rate: ");
+        double interestRate = input.nextDouble();
+
+        System.out.print("Enter number of months: ");
+        int numberOfMonths = input.nextInt();
+
+        for(int i = 1 ; i <= numberOfMonths; i++) {
+
+            monthlyInterest = interestRate / 12 / 100 ;
+            amount = amount + (amount * monthlyInterest);
+            count += 1;
+
+            output += "\nMonth " + count + " value is $" + String.format("%.2f", amount);
+
+        }
+        System.out.println(output);
+    }
+}
