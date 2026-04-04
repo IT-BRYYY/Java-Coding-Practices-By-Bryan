49b222e78ca6a66a6612d191dc77f4be10632eaa
Bryan <sktbryann@gmail.com>
2026-04-04 22:51:13 +0800

    Change logic and code in gretest common divisor using methods

--- src/GreatestCommonDivisor.java
+++ src/GreatestCommonDivisor.java
@@ -8,12 +8,19 @@ public class GreatestCommonDivisor {
         System.out.print("Enter second integer: ");
         int n2 = input.nextInt();
 
-        int gcd = 1;
+        System.out.println("The greatest common divisor for " + n1 + " and " + n2 + " is " + gcd(n1,n2));
+    }
 
-        for(int k= 2; k <= n1 && k <= n2; k++){
-            if(n1 % k == 0 && n2 % k ==0)
+    public static int gcd (int n1, int n2){
+        int gcd = 1;
+        int k = 2;
+
+        while(k <= n1 && k <= n2){
+            if(n1 % k == 0 && n2 % k == 0 )
                 gcd = k;
+            k++;
+
         }
-        System.out.println("The greatest common divisor for " + n1 + " and " + n2 + " is " + gcd);
+        return gcd;
     }
 }
