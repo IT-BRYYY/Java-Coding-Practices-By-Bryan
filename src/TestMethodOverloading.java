1277069244a84dfef0772ab8d74c10ad067d42fd
Bryan <sktbryann@gmail.com>
2026-04-05 00:00:52 +0800

    Fixing logic in Test Method Overloading

--- src/TestMethodOverloading.java
+++ src/TestMethodOverloading.java
@@ -13,22 +13,16 @@ public class TestMethodOverloading {
 
     /** Return the max of two int values */
     public static int max(int num1, int num2) {
-        if (num1 > num2)
-            return num1;
-        else
-            return num2;
+        return Math.max(num1, num2);
     }
 
     /** Find the max of two double values */
     public static double max(double num1, double num2) {
-        if (num1 > num2)
-            return num1;
-        else
-            return num2;
+        return Math.max(num1, num2);
     }
 
     /** Return the max of three double values */
     public static double max(double num1, double num2, double num3) {
-        return max(max(num1, num2), num3);
+        return Math.max(max(num1, num2), num3);
     }
 }
