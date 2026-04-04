f1b2ef1455a2335528b1762716cd87f725b9011f
Bryan <sktbryann@gmail.com>
2026-04-04 23:23:49 +0800

    Change logic and code in Prime numbers using methods

--- src/PrimeNumbers.java
+++ src/PrimeNumbers.java
@@ -1,18 +1,33 @@
 public class PrimeNumbers {
     public static void main(String []args){
-        System.out.println("Prime numbers from 1 to 50: ");
+        System.out.println("The first 50 prime numbers are \n:");
+        printPrimeNumbers(50);
+    }
 
-        for(int num = 2; num<= 50; num++){
-            boolean isPrime = true;
-            for(int i = 2; i <=Math.sqrt(num); i++){
-                if(num % i == 0){
-                    isPrime = false;
-                    break;
+    public static void printPrimeNumbers(int numberOfPrimes){
+        final int NUMBER_OF_PRIMES_PER_LINE = 10;
+        int count = 0;
+        int number = 2;
 
-                }
+        while(count < numberOfPrimes){
+            if(isPrime(number)){
+                count++;
             }
-            if(isPrime) System.out.print(num + " ");
+            if(count %  NUMBER_OF_PRIMES_PER_LINE == 0){
+                System.out.printf("%-5d\n", number);
+            }else {
+                System.out.printf("%-5d", number);
+            }
+            number++;
         }
+    }
+    public static boolean isPrime(int number){
+        for(int divisor = 2; divisor <= number / 2 ;divisor++){
+            if(number % divisor == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
 
