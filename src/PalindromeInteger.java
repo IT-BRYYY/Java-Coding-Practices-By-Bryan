96abcdc82c8afa39eb1965fc61f2a82c29577d34
IT-BRYYY <178906422+IT-BRYYY@users.noreply.github.com>
2026-04-12 23:55:56 +0800

    Add Palindrome Integer using methods

---
+++ src/PalindromeInteger.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class PalindromeInteger {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter an integer: ");
+        int number = input.nextInt();
+
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome!");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+
+    public static int reverse(int number) {
+        int reversed = 0;
+        while (number > 0) {
+            int digit = number % 10;
+            reversed = reversed * 10 + digit;
+            number /= 10;
+        }
+        return reversed;
+    }
+
+    public static boolean isPalindrome(int number) {
+        return reverse(number) == number;
+    }
+}
