a5edbb447ccd88593f0d8d8f6f1dd91114df17bf
IT-BRYYY <178906422+IT-BRYYY@users.noreply.github.com>
2026-06-25 00:22:55 +0800

    Add Test Stock class to test the program

---
+++ src/OOP/TestStock.java
@@ -0,0 +1,14 @@
+package OOP;
+
+public class TestStock {
+    public static void main(String[] args) {
+
+        Stock stock1 = new Stock("ORCL", "Oracle Corporation");
+        stock1.setPreviousClosingPrice(34.5);
+        stock1.setCurrentPrice(34.35);
+
+        stock1.showResult();
+
+
+    }
+}
