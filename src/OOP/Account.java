76ebef910d68c3f56cfb0c7e9338fcf60df577b0
IT-BRYYY <178906422+IT-BRYYY@users.noreply.github.com>
2026-06-27 00:05:09 +0800

    Add Account class with dateCreated field and business logic

---
+++ src/OOP/Account.java
@@ -0,0 +1,54 @@
+package OOP;
+
+public class Account {
+    private int id;
+    private double balance;
+    private double annualInterestRate ;
+    private java.util.Date dateCreated;
+
+    Account(){
+        this.id = 0;
+        this.balance = 0;
+        this.annualInterestRate = 0;
+        this.dateCreated = new java.util.Date();
+
+    }
+    Account(int id, double balance, double annualInterestRate){
+        this.id = id;
+        this.balance = balance;
+        this.annualInterestRate = annualInterestRate;
+        this.dateCreated = new java.util.Date();
+    }
+    public java.util.Date getDateCreated(){
+        return dateCreated;
+    }
+    public int getId(){
+        return id;
+    }
+    public double getBalance(){
+        return balance;
+    }
+    public double getAnnualInterestRate(){
+        return annualInterestRate;
+    }
+
+    public double getMonthlyInterestRate(){
+        return annualInterestRate / 12;
+    }
+
+    public double getMonthlyInterest(){
+        return balance * (getMonthlyInterestRate() / 100);
+    }
+
+    public void withdraw(double amount){
+        if(balance >= amount){
+            balance -= amount;
+        }
+    }
+    public void deposit(double amount){
+        if(amount > 0){
+            balance += amount;
+        }
+    }
+
+}
