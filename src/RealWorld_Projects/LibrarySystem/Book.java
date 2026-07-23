fdd5a8f4ab173a5cebd7768e3b5c549e04a01628
IT-BRYYY <178906422+IT-BRYYY@users.noreply.github.com>
2026-07-23 21:27:41 +0800

    refactor(Book): Encapsulate fields with private access modifiers
    - Make all instance variables private (title, author, yearPublished, isCheckedOut)
    - Add getters and setters for all fields
    - Add validation in setYearPublished() to prevent negative years
    - Maintain existing constructor chaining pattern
    - Improve data protection and follow encapsulation best practices

--- src/RealWorld_Projects/LibrarySystem/Book.java
+++ src/RealWorld_Projects/LibrarySystem/Book.java
@@ -1,10 +1,10 @@
 package RealWorld_Projects.LibrarySystem;
 
 public class Book {
-    String title;
-    String author;
-    int yearPublished;
-    boolean isCheckedOut;
+    private String title;
+    private String author;
+    private int yearPublished;
+    private boolean isCheckedOut;
 
     public Book(String title, String author, int yearPublished, boolean isCheckedOut){
         this.title = title;
@@ -17,6 +17,35 @@ public class Book {
     }
     public Book(String title){
         this(title,"Unknown",2020,false);
+    }
+    public String getTitle(){
+        return title;
+    }
+    public String getAuthor(){
+        return author;
+    }
+    public int getYearPublished(){
+        return yearPublished;
+    }
+    public boolean isCheckedOut(){
+        return isCheckedOut;
+    }
+
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public void setAuthor(String author){
+        this.author = author;
+    }
+    public void setYearPublished(int yearPublished){
+        if(yearPublished > 0){
+            this.yearPublished = yearPublished;
+        }else
+            System.out.println("Invalid year Published");
+    }
+    public void setIsCheckedOut(boolean isCheckedOut){
+        this.isCheckedOut = isCheckedOut;
     }
 
     public void displayInfo(){
