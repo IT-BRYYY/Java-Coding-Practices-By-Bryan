16f1b01b0f51055de0f1de7429794043cf4753d6
IT-BRYYY <178906422+IT-BRYYY@users.noreply.github.com>
2026-07-31 23:40:37 +0800

    Implement Student subclass extending Person with class status constants (Freshman, Sophomore, Junior, Senior) and proper constructor using super() to reuse parent class initialization"

---
+++ src/RealWorld_Projects/HRMS/Student.java
@@ -0,0 +1,25 @@
+package RealWorld_Projects.HRMS;
+
+public class Student extends Person {
+    public static final String FRESHMAN = "Freshman";
+    public static final String Sophomore = "Sophomore";
+    public static final String JUNIOR = "Junior";
+    public static final String SENIOR = "Senior";
+
+    private String classStatus;
+
+    public Student(String name, String address, String phoneNumber, String email, String classStatus){
+        super(name, address, phoneNumber, email);
+        this.classStatus = classStatus;
+    }
+
+    public String getClassStatus(){
+        return classStatus;
+    }
+    public void setClassStatus(String classStatus){
+        this.classStatus = classStatus;
+    }
+    public String toString(){
+        return "Student: " + getName();
+    }
+}
